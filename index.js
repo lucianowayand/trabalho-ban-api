69eb8ce9b81d4df03e145c43fabe3d31176cd862
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-07-20 16:55:29 -0300

    docs: added insomnia file and comments on the index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.get('/clientes', async (req, res) => {
-
+    // Retorna uma lista de todos os clientes
     try {
         const clientes = await prisma.$queryRaw(
             Prisma.sql`SELECT * FROM "Clientes"`
@@ -21,7 +21,7 @@ app.get('/clientes', async (req, res) => {
 })
 
 app.post('/clientes', async (req, res) => {
-
+    // Cria novo cliente pelos valores recebidos no body em json
     try {
         const clientes = await prisma.$queryRaw(
             Prisma.sql`INSERT INTO "Clientes" VALUES (${req.body.cpf}, ${req.body.nomeCliente}, ${req.body.telefone}) `
@@ -35,7 +35,7 @@ app.post('/clientes', async (req, res) => {
 
 
 app.get('/clientes/animais', async (req, res) => {
-
+    // Retorna uma lista de animais e seus donos
     try {
         const resultado = await prisma.$queryRaw(
             Prisma.sql`SELECT * FROM "Clientes" JOIN "Animais" ON cpf="cpfDono"`
@@ -48,7 +48,7 @@ app.get('/clientes/animais', async (req, res) => {
 })
 
 app.get('/animais', async (req, res) => {
-
+    // Retorna uma lista de todos os animais
     try {
         const animais = await prisma.$queryRaw(
             Prisma.sql`SELECT * FROM "Animais"`
@@ -61,7 +61,7 @@ app.get('/animais', async (req, res) => {
 })
 
 app.post('/animais', async (req, res) => {
-
+    // Cria novo animal pelos valores recebidos no body em json
     try {
         const animais = await prisma.$queryRaw(
             Prisma.sql`INSERT INTO "Animais" ("nomeAnimal", tamanho, tipo, "dataNasc", "cpfDono") VALUES (${req.body.nomeAnimal}, ${req.body.tamanho}, ${req.body.tipo}, ${new Date(req.body.dataNasc)}, ${req.body.cpfDono}) `
@@ -74,14 +74,13 @@ app.post('/animais', async (req, res) => {
 })
 
 app.get('/animais/tipo/:tipo', async (req, res) => {
-
+    // Retorna a quantidade de donos de um tipo de animal definido
     try {
         const animais = await prisma.$queryRaw(
             Prisma.sql`SELECT COUNT(*) FROM (SELECT * FROM "Clientes" WHERE cpf = any (SELECT "cpfDono" FROM "Animais" WHERE tipo = ${req.params.tipo})) as asd`
         )
         res.send("Quantidade de donos: "+Number(animais[0].count))
     } catch (e) {
-        console.log(e)
         res.json({ msg: e })
     }
 
