e10eab25b049a64fe8689468703a641b01d1ecd0
GabrielFJunkes <69057368+GabrielFJunkes@users.noreply.github.com>
2022-07-20 19:43:41 +0000

    Add ultimos dois requerimentos (#4)
    * add diagrama do banco
    
    * Add banco e docker
    
    * Arruma schema
    
    * Add .env, fix chema e sql
    
    * Add post e get de animais e clientes
    
    * Add ultimos dois requisitos
    
    Co-authored-by: Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const app = express()
+app.use(bodyParser.json())
 
 app.get('/clientes', async (req, res) => {
 
@@ -12,7 +15,33 @@ app.get('/clientes', async (req, res) => {
         )
         res.json(clientes)
     } catch (e) {
-        console.log(e)
+        res.json({ msg: e })
+    }
+
+})
+
+app.post('/clientes', async (req, res) => {
+
+    try {
+        const clientes = await prisma.$queryRaw(
+            Prisma.sql`INSERT INTO "Clientes" VALUES (${req.body.cpf}, ${req.body.nomeCliente}, ${req.body.telefone}) `
+        )
+        res.json({msg: "Criado com sucesso"})
+    } catch (e) {
+        res.json({ msg: e })
+    }
+
+})
+
+
+app.get('/clientes/animais', async (req, res) => {
+
+    try {
+        const resultado = await prisma.$queryRaw(
+            Prisma.sql`SELECT * FROM "Clientes" JOIN "Animais" ON cpf="cpfDono"`
+        )
+        res.json(resultado)
+    } catch (e) {
         res.json({ msg: e })
     }
 
@@ -20,11 +49,42 @@ app.get('/clientes', async (req, res) => {
 
 app.get('/animais', async (req, res) => {
 
-    const animais = await prisma.$queryRaw`Select * From Animais`
-
-    res.json(animais)
+    try {
+        const animais = await prisma.$queryRaw(
+            Prisma.sql`SELECT * FROM "Animais"`
+        )
+        res.json(animais)
+    } catch (e) {
+        res.json({ msg: e })
+    }
 
 })
 
+app.post('/animais', async (req, res) => {
+
+    try {
+        const animais = await prisma.$queryRaw(
+            Prisma.sql`INSERT INTO "Animais" ("nomeAnimal", tamanho, tipo, "dataNasc", "cpfDono") VALUES (${req.body.nomeAnimal}, ${req.body.tamanho}, ${req.body.tipo}, ${new Date(req.body.dataNasc)}, ${req.body.cpfDono}) `
+        )
+        res.json({msg: "Criado com sucesso"})
+    } catch (e) {
+        res.json({ msg: e })
+    }
+
+})
+
+app.get('/animais/tipo/:tipo', async (req, res) => {
+
+    try {
+        const animais = await prisma.$queryRaw(
+            Prisma.sql`SELECT COUNT(*) FROM (SELECT * FROM "Clientes" WHERE cpf = any (SELECT "cpfDono" FROM "Animais" WHERE tipo = ${req.params.tipo})) as asd`
+        )
+        res.send("Quantidade de donos: "+Number(animais[0].count))
+    } catch (e) {
+        console.log(e)
+        res.json({ msg: e })
+    }
+
+})
 
 const server = app.listen(3000)
