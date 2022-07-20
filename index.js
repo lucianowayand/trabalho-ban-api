d31d76c11bee3a2f23a14b77ea7169ef7a8fac25
GabrielFJunkes <gabriel.f.junkes@gmail.com>
2022-07-20 16:39:35 -0300

    Add ultimos dois requisitos

--- index.js
+++ index.js
@@ -24,9 +24,23 @@ app.post('/clientes', async (req, res) => {
 
     try {
         const clientes = await prisma.$queryRaw(
-            Prisma.sql`INSERT INTO "Clientes" VALUES (${req.body.cpf}, ${req.body.nome}, ${req.body.telefone}) `
+            Prisma.sql`INSERT INTO "Clientes" VALUES (${req.body.cpf}, ${req.body.nomeCliente}, ${req.body.telefone}) `
         )
-        res.json(clientes)
+        res.json({msg: "Criado com sucesso"})
+    } catch (e) {
+        res.json({ msg: e })
+    }
+
+})
+
+
+app.get('/clientes/animais', async (req, res) => {
+
+    try {
+        const resultado = await prisma.$queryRaw(
+            Prisma.sql`SELECT * FROM "Clientes" JOIN "Animais" ON cpf="cpfDono"`
+        )
+        res.json(resultado)
     } catch (e) {
         res.json({ msg: e })
     }
@@ -50,14 +64,30 @@ app.post('/animais', async (req, res) => {
 
     try {
         const animais = await prisma.$queryRaw(
-            Prisma.sql`INSERT INTO "Animais" (nome, tamanho, tipo, "dataNasc", "cpfDono") VALUES (${req.body.nome}, ${req.body.tamanho}, ${req.body.tipo}, ${new Date(req.body.dataNasc)}, ${req.body.cpfDono}) `
+            Prisma.sql`INSERT INTO "Animais" ("nomeAnimal", tamanho, tipo, "dataNasc", "cpfDono") VALUES (${req.body.nomeAnimal}, ${req.body.tamanho}, ${req.body.tipo}, ${new Date(req.body.dataNasc)}, ${req.body.cpfDono}) `
         )
-        res.json(animais)
+        res.json({msg: "Criado com sucesso"})
     } catch (e) {
         res.json({ msg: e })
     }
 
 })
+
+app.get('/animais/tipo/:tipo', async (req, res) => {
+
+    try {
+        const animais = await prisma.$queryRaw(
+            Prisma.sql`SELECT COUNT(*) FROM (SELECT * FROM "Clientes" WHERE cpf = any (SELECT "cpfDono" FROM "Animais" WHERE tipo = ${req.params.tipo})) as asd`
+        )
+        res.send("Quantidade de donos: "+Number(animais[0].count))
+    } catch (e) {
+        console.log(e)
+        res.json({ msg: e })
+    }
+
+})
+
+
 
 
 const server = app.listen(3000)
